d52c19f05df648c7f4c576427d5d2e14eb82be38
clean17 <piw940317@gmail.com>
2023-03-28 07:51:14 +0900

    하단 화면 추가

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -5,6 +5,92 @@ class ShoppingCartDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingAndReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailNameAndPrice() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),          Text(
+            "\$699",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailRatingAndReviewCount() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailColorOptions() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color Options"),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailIcon(Color mColor) {
+    return SizedBox();
+  }
+
+  Widget _buildDetailButton(BuildContext context) {
     return SizedBox();
   }
 }
